2ae60078bd2067690c776bc31543946fe9c93f99
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-03-27 14:27:05 +0100

    update

--- config/git/link-git.bat
+++ config/git/link-git.bat
@@ -1,5 +1,7 @@
 @echo off
 pushd %userprofile%
 del .gitconfig >nul 2>&1
+del .gitconfig-ibm >nul 2>&1
 mklink .gitconfig %dotfiles%\config\git\.gitconfig
+mklink .gitconfig-ibm %dotfiles%\config\git\.gitconfig-ibm
 popd
