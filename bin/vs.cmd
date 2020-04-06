61280407dbc60557d9edbd3d74a91ad461b98a88
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-04-06 16:12:16 +0200

    update

--- bin/vs.cmd
+++ bin/vs.cmd
@@ -1,7 +1,13 @@
 @if not defined _echo echo off
+if "%1" == "" (
+    set arch=x64
+) else (
+    set arch=%1
+)
+
 for /f "usebackq delims=" %%i in (`vswhere.exe -prerelease -latest -property installationPath`) do (
-    if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-        %comspec% /k "%%i\Common7\Tools\vsdevcmd.bat" %*
+    if exist "%%i\VC\Auxiliary\Build\vcvarsall.bat" (
+        %comspec% /k "%%i\VC\Auxiliary\Build\vcvarsall.bat" %ARCH% %*
         exit /b
     )
 )
