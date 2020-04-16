a48ae0ed7c571c077899bdc2651e488b441fe6e2
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-04-16 11:33:08 +0200

    update

--- bin/vs.cmd
+++ bin/vs.cmd
@@ -1,8 +1,20 @@
-@if not defined _echo echo off
+@echo off
+
 if "%1" == "" (
     set arch=x64
 ) else (
     set arch=%1
 )
 
-cmd.exe /k call set prompt=(%arch%) $P$G ^&^& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %arch%
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath`) do (
+    if exist "%%i" (
+        set vcvarsall=%%i\VC\Auxiliary\Build\vcvarsall.bat
+    )
+)
+
+if "%vcvarsall%" == "" (
+    echo Unable to find Visual Studio installation
+    exit /b 1
+)
+
+cmd.exe /k set prompt=(%arch%) $P$G ^&^& call "%vcvarsall%" %arch%
