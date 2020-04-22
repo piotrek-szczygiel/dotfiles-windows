e311f7c273d4355b9dc38bd475fe4b5e4794f379
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-04-22 10:21:59 +0200

    update

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -39,12 +39,6 @@
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 
-(set-face-attribute 'default nil
-		    :family "Consolas"
-		    :height 120
-		    :weight 'normal
-		    :width 'normal)
-
 (defun sensible-defaults/reset-text-size ()
   (interactive)
   (text-scale-set 0))
@@ -67,3 +61,16 @@
 
 (when (or window-system (daemonp))
   (setq default-frame-alist '((width . 120) (height . 40))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
