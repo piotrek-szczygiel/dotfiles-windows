1c60e783c7a2762df11a2dafe3451c62de4f3005
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-05-03 22:11:34 +0200

    update

--- config/sublime/link-sublime.bat
+++ config/sublime/link-sublime.bat
@@ -3,6 +3,8 @@ mkdir %userprofile%\scoop\persist\sublime-text\Data\Packages\User
 pushd %userprofile%\scoop\persist\sublime-text\Data\Packages\User
 del Preferences.sublime-settings >nul 2>&1
 del "Package Control.sublime-settings" >nul 2>&1
+del Terminal.sublime-settings >nul 2>&1
 mklink Preferences.sublime-settings %dotfiles%\config\sublime\Preferences.sublime-settings
 mklink "Package Control.sublime-settings" "%dotfiles%\config\sublime\Package Control.sublime-settings"
+mklink Terminal.sublime-settings %dotfiles%\config\sublime\Terminal.sublime-settings
 popd
