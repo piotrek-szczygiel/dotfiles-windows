ddfcf3582e6376b4c45ac4f3e7b2c4d869fff2ef
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-06-10 16:19:02 +0200

    update

--- config/neovim/link-neovim.bat
+++ config/neovim/link-neovim.bat
@@ -2,5 +2,5 @@
 mkdir %localappdata%\nvim >nul 2>&1
 pushd %localappdata%\nvim
 del init.vim >nul 2>&1
-mklink init.vim %dotfiles%\config\nvim\init.vim
+mklink init.vim %dotfiles%\config\neovim\init.vim
 popd
