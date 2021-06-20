f868b5ba3f6844c2a1031d4dc39d6ee8b37112ba
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-06-21 00:15:25 +0200

    update

--- config/sublime-text/link-sublime-text.bat
+++ config/sublime-text/link-sublime-text.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir "%appdata%\Sublime Text\Packages\User" >nul 2>&1
 pushd "%appdata%\Sublime Text\Packages\User"
-del *.sublime-settings" >nul 2>&1
-for %%f in ("%dotfiles%\config\sublime-text\*.sublime-settings") do ( mklink %%~nf %%f )
+del *.sublime-settings >nul 2>&1
+for %%f in ("%dotfiles%\config\sublime-text\*.sublime-settings") do (mklink "%%~nxf" "%%f")
 popd
