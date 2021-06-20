692c15f6d147b6c4dc726e1d82599947e0a4f308
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-06-21 00:12:41 +0200

    update

--- config/sublime-text/link-sublime-text.bat
+++ config/sublime-text/link-sublime-text.bat
@@ -1,10 +1,6 @@
 @echo off
 mkdir "%appdata%\Sublime Text\Packages\User" >nul 2>&1
 pushd "%appdata%\Sublime Text\Packages\User"
-del "Preferences.sublime-settings" >nul 2>&1
-del "Package Control.sublime-settings" >nul 2>&1
-del "Default (Windows).sublime-keymap" >nul 2>&1
-mklink "Preferences.sublime-settings" "%dotfiles%\config\sublime-text\Preferences.sublime-settings"
-mklink "Package Control.sublime-settings" "%dotfiles%\config\sublime-text\Package Control.sublime-settings"
-mklink "Default (Windows).sublime-keymap" "%dotfiles%\config\sublime-text\Default (Windows).sublime-keymap"
+del *.sublime-settings" >nul 2>&1
+for %%f in ("%dotfiles%\config\sublime-text\*.sublime-settings") do ( mklink %%~nf %%f )
 popd
