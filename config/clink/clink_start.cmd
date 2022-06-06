e596ad610df079c80e8ebac66337113495be31cc
Piotr Szczygieł <szczygiel.p@gmail.com>
2022-06-06 10:49:43 +0200

    update

--- config/clink/clink_start.cmd
+++ config/clink/clink_start.cmd
@@ -1,22 +1,7 @@
 @echo off
 
 doskey e=explorer "%%cd%%"
-
 doskey cat=bat --paging=never -p $*
-
-doskey q=exit /b 0
-doskey exit=echo Use 'q' to exit
-
-doskey l=exa --icons --group-directories-first $*
-doskey ls=exa --icons --group-directories-first $*
-doskey ll=exa --icons --group-directories-first -l $*
-doskey la=exa --icons --group-directories-first -la $*
-
-doskey sudo=gsudo --wait $*
-
-doskey tg=terragrunt $*
-
-doskey md=mkdir ""$*"" $T cd ""$*""
 
 doskey ga=git add $*
 doskey gus=git restore --staged $*
@@ -30,3 +15,17 @@ doskey gl=git pull $*
 doskey glg=git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit $*
 doskey gp=git push $*
 doskey gs=git status $*
+
+doskey l=exa --icons --group-directories-first $*
+doskey ls=exa --icons --group-directories-first $*
+doskey ll=exa --icons --group-directories-first -l $*
+doskey la=exa --icons --group-directories-first -la $*
+
+doskey md=mkdir ""$*"" $T cd ""$*""
+doskey sudo=gsudo --wait $*
+doskey tg=terragrunt $*
+doskey zf=z -I $*
+
+
+doskey q=exit /b 0
+doskey exit=echo Use 'q' to exit
